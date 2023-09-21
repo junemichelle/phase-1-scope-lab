07da5ba9765da349220d0f1397cb7369b14e310a
michelle <junemichelle20@gmail.com>
2023-09-21 20:56:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName= "bob";
+function upperCaseCustomerName(){
+customerName= customerName.toUpperCase();
+//console.log(upperCaseCustomerName
+}
+function setBestCustomer(){
+  bestCustomer="not bob";
+}
+function overwriteBestCustomer(){
+bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer="someone";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer= "someone else";
+}
